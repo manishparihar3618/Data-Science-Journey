ddf9a7c77140b91fdc71a81acef446cf2b425452
manishparihar3618 <manishparihra3618@gmail.com>
2026-06-01 15:13:58 +0530

    Initial Flask application

---
+++ 10_Data_Science_Tools/flask-basics/Flask.py
@@ -0,0 +1,33 @@
+from flask import Flask
+'''
+It creates an instance of the Flask class, which will be our WSGI application. 
+'''
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def welcome():
+    return "Welcome to this flask course"
+
+
+if __name__ == '__main__':
+    app.run() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
